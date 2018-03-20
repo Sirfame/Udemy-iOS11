8c3ee1d556d765b7ed4ff2cb25b4d7a1f12d4643
Sirfame Lin <sifame@hotmail.com>
2018-03-19 21:58:44 -0700

    migrating repos

---
+++ My First App/My First App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Sirfame Lin on 1/22/18.
+//  Copyright © 2018 Sirfame Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Sirfame");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
