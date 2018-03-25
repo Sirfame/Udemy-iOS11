31bf9a6b8e4353685e74fa52c95f794af0fdcb8e
Sirfame Lin <sirfame@hotmail.com>
2018-03-25 15:06:47 -0700

    primitive data types in swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,7 +2,24 @@
 
 import UIKit
 
-var str = "Sirfame";
-print(str);
-var f = 5;
+var name = "Sirfame";
+var myInt = 25;
+print(myInt + 100);
+myInt += 1
+
+var age: Int = 24;
+print("my name is \(name) and i am \(age)");
+
+var dub: Double = 5.76
+var int: Int = 8
+
+print(Double(age) * dub);
+
+var bool = true;
+
+if bool {
+    print(name)
+}
+
+
 
