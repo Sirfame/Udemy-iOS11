7fd063484f4e6a5b6f921ca7196b987f65d0b181
Sirfame Lin <sirfame@hotmail.com>
2018-03-25 18:33:50 -0700

    added if/else

--- Playgrounds/3_IfStatements.playground/Contents.swift
+++ Playgrounds/3_IfStatements.playground/Contents.swift
@@ -4,4 +4,16 @@ if age > 23 {
     print(true);
 }
 
+let username = "sf"
+let password = "pasjs"
+
+if username == "sf" && password == "pass" {
+    print("both correct")
+} else if username != "sf" && password == "pass" {
+    print("username wrong")
+} else if username == "sf" {
+    print("pass wrong")
+} else {
+    print("both wrong")
+}
 
