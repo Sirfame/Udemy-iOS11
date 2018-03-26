84faf3cfef2790975f982def89a48e2c5d6d1235
Sirfame Lin <sirfame@hotmail.com>
2018-03-26 08:35:28 -0700

    prime number app

--- Is It Prime/Is It Prime/ViewController.swift
+++ Is It Prime/Is It Prime/ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBAction func checkPrime(_ sender: Any) {
+        if let stringNumber = input.text {
+            if let intNumber = Int(stringNumber) {
+                if isPrime(n: intNumber) {
+                    answerLabel.text = "\(intNumber) is a prime number";
+                } else {
+                    answerLabel.text = "\(intNumber) is not a prime number"
+                }
+            }
+        }
+    }
+    
+    func isPrime(n : Int) -> Bool {
+        if n < 2 {
+            return false;
+        }
+        var divisor = 2;
+        while divisor <= n / 2 {
+            if n % divisor == 0 {
+                return false;
+            }
+            divisor += 1;
+        }
+        return true;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
