a967989686c5225a56b5a39065cb8b5d7bdec38e
Sirfame Lin <sirfame@hotmail.com>
2018-03-27 15:00:30 -0700

    bare bones navbar and toolbar demo

---
+++ Menu Bars/Menu Bars/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by Sirfame Lin on 3/27/18.
+//  Copyright © 2018 Sirfame Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func CameraButtonPressed(_ sender: Any) {
+        print("Camera button pressed")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
