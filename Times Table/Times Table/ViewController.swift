83723ec3ffbbbd26db96301777038644668186a5
Sirfame Lin <sirfame@hotmail.com>
2018-03-29 18:20:56 -0700

    multiplication table view

--- Times Table/Times Table/ViewController.swift
+++ Times Table/Times Table/ViewController.swift
@@ -1,29 +1,33 @@
 //
 //  ViewController.swift
-//  TableDemoOneToFifty
+//  Times Table
 //
-//  Created by Sirfame Lin on 3/28/18.
+//  Created by Sirfame Lin on 3/29/18.
 //  Copyright © 2018 Sirfame Lin. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var table: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 50;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell");
         
-        cell.textLabel?.text = String(indexPath.row + 1);
+        cell.textLabel?.text = "\(Int(slider.value)) x \(indexPath.row + 1) = \(Int(slider.value) * (indexPath.row + 1))";
         
         return cell;
     }
     
-
+    @IBAction func sliderChanged(_ sender: Any) {
+        table.reloadData();
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
