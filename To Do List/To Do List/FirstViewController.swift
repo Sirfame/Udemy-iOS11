3c6c93dfded33ab9c95e88fd8dbaf9036d91a49e
Sirfame Lin <sirfame@hotmail.com>
2018-04-01 16:47:55 -0700

    optimized to do app

--- To Do List/To Do List/FirstViewController.swift
+++ To Do List/To Do List/FirstViewController.swift
@@ -10,27 +10,56 @@ import UIKit
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var ToDoList = UserDefaults.standard.object(forKey: "ToDoList") as? Array<String>
+    // Original attempt
+    //var ToDoList = UserDefaults.standard.object(forKey: "ToDoList") as? Array<String>
     
+    // Second attempt
+    var ToDoList : [String] = [];
     @IBOutlet weak var table: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(ToDoList?.count);
+        // First attempt
+        /*
         if ToDoList != nil {
             return ToDoList!.count;
         }
         return 0;
+        */
+        
+        // Second attempt
+        return ToDoList.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Original Attempt
+        /*
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell");
         if let task = ToDoList![indexPath.row] as? String  {
             cell.textLabel?.text = task;
         }
         return cell;
+         */
+        
+        // Second attempt
+        /*
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell");
+        var cellLabel = "";
+        if let tempLabel = ToDoList[indexPath.row] as? String {
+            cellLabel = tempLabel;
+        }
+        cell.textLabel?.text = cellLabel;
+        return cell;
+         */
+        
+        // Third attempt - no longer need to check, we can be sure items will be a string
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell");
+        cell.textLabel?.text = ToDoList[indexPath.row];
+        return cell;
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // First attempt
+        /*
         if editingStyle == .delete {
             print("Deleted")
             ToDoList?.remove(at: indexPath.row);
@@ -38,17 +67,43 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
             UserDefaults.standard.set(ToDoList, forKey: "ToDoList")
             self.table.deleteRows(at: [indexPath], with: .automatic)
         }
+        */
+        
+        // Second attempt
+        if editingStyle == .delete {
+            print("Deleted")
+            ToDoList.remove(at: indexPath.row);
+            //self.catNames.remove(at: indexPath.row)
+            table.reloadData();
+            UserDefaults.standard.set(ToDoList, forKey: "ToDoList")
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        // First attempt
+        /*
         print("Table view appeared")
         ToDoList = UserDefaults.standard.object(forKey: "ToDoList") as? Array<String>
+        table.reloadData();
+         */
+        
+        // Second attempt
+        let itemsObject = UserDefaults.standard.object(forKey: "ToDoList");
+        if let tempItems = itemsObject as? [String] {
+            ToDoList = tempItems;
+        }
         table.reloadData();
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //UserDefaults.standard.removeObject(forKey: "ToDoList");
+        // Second Attempt
+        let itemsObject = UserDefaults.standard.object(forKey: "ToDoList");
+        if let tempItems = itemsObject as? [String] {
+            ToDoList = tempItems;
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- To Do List/To Do List/SecondViewController.swift
+++ To Do List/To Do List/SecondViewController.swift
@@ -13,6 +13,8 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var toDoInput: UITextField!
     
     @IBAction func AddItemButton(_ sender: Any) {
+        // My original attempt
+        /*
         if let inputText = toDoInput.text {
             if var arr = UserDefaults.standard.object(forKey: "ToDoList") as? Array<String> {
                 arr.append(inputText);
@@ -22,11 +24,27 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
             }
             toDoInput.text = "";
         }
+        */
+        
+        // Second attempt
+        let itemsObject = UserDefaults.standard.object(forKey: "ToDoList");
+        var items:[String]
+        if let tempItems = itemsObject as? [String] {
+            items = tempItems;
+            items.append(toDoInput.text!)
+        } else {
+            items = [toDoInput.text!]
+        }
+        UserDefaults.standard.set(items, forKey: "ToDoList");
+        toDoInput.text = "";
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Ending up not needing this code in viewDidLoad, since our add button will do the checking.
+        /*
         if UserDefaults.standard.object(forKey: "ToDoList") == nil {
             let ToDoList = [String]();
             UserDefaults.standard.set(ToDoList, forKey: "ToDoList")
@@ -34,6 +52,7 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         if let arr = UserDefaults.standard.object(forKey: "ToDoList") as? Array<String> {
             print(arr);
         }
+         */
     }
 
     override func didReceiveMemoryWarning() {
