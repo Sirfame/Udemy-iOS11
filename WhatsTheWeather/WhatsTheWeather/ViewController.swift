4d9f6617505057cf503ac43a466da6daa7a7a5ae
Sirfame Lin <sirfame@hotmail.com>
2018-04-02 19:30:35 -0700

    completed weather app

--- WhatsTheWeather/WhatsTheWeather/ViewController.swift
+++ WhatsTheWeather/WhatsTheWeather/ViewController.swift
@@ -10,38 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        if let url = URL(string: "https://www.weather-forecast.com/locations/Seattle/forecasts/latest.com") {
-            let request = NSMutableURLRequest(url: url);
-            let task = URLSession.shared.dataTask(with: request as URLRequest) {
-                data, response, error in
-                
-                if let error = error {
-                    print(error)
-                } else {
-                    if let unwrappedData = data {
-                        if let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue) {
-                            //print(dataString);
-                            var arr = dataString.components(separatedBy: "(1&ndash;3 days)</span><p class=\"b-forecast__table-description-content\"><span class=\"phrase\">");
-                            let temp = NSString(string: "\(arr[1])");
-                            var arr2 = temp.components(separatedBy: "</span>");
-                            let weather = NSString(string: "\(arr2[0])");
-                            print(NSString(string: weather).replacingOccurrences(of: "&deg;", with: "°"))
-                            DispatchQueue.main.sync(execute: {
-                                // Update UI
-                            });
+    @IBOutlet var userInput: UITextField!
+    @IBOutlet var display: UILabel!
+    @IBAction func submit(_ sender: Any) {
+        
+        if let input = userInput.text {
+            var city = input;
+            if input.contains(" ") {
+                city = input.replacingOccurrences(of: " ", with: "-");
+            }
+            let rawUrl = "https://www.weather-forecast.com/locations/" + city + "/forecasts/latest.com"
+            if let url = URL(string: rawUrl) {
+                let request = NSMutableURLRequest(url: url);
+                let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                    data, response, error in
+                    
+                    if let error = error {
+                        print(error)
+                    } else {
+                        if let unwrappedData = data {
+                            if let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue) {
+                                print(dataString);
+                                if dataString.contains("Weather-Forecast.com - global weather source.") {
+                                    DispatchQueue.main.sync(execute: {
+                                        // Update UI
+                                        self.display.text = "\(input) was not found, sorry."
+                                    });
+                                } else {
+                                    var arr = dataString.components(separatedBy: "(1&ndash;3 days)</span><p class=\"b-forecast__table-description-content\"><span class=\"phrase\">");
+                                    let temp = NSString(string: "\(arr[1])");
+                                    var arr2 = temp.components(separatedBy: "</span>");
+                                    let weather = NSString(string: "\(arr2[0])");
+                                    let displayString = NSString(string: weather).replacingOccurrences(of: "&deg;", with: "°");
+                                    print(displayString);
+                                    
+                                    DispatchQueue.main.sync(execute: {
+                                        // Update UI
+                                        self.display.text = NSString(string: weather).replacingOccurrences(of: "&deg;", with: "°");
+                                    });
+                                }
+                            }
                         }
                     }
                 }
+                task.resume();
             }
-            
-            task.resume();
         }
-        
-        
-        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
