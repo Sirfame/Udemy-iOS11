1e7ca5e4b9b04c823fc0faaeecc127b14bde490f
Sirfame Lin <sirfame@hotmail.com>
2018-06-15 07:08:37 -0700

    tic tac toe progress

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Sirfame Lin on 4/13/18.
+//  Copyright © 2018 Sirfame Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("button pressed")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
