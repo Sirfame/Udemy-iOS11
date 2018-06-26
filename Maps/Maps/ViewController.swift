895a2101e3de1194071d1494ef203a9c88c0dd34
Sirfame Lin <sirfame@hotmail.com>
2018-06-25 21:06:59 -0700

    Playing with Map Kit View

---
+++ Maps/Maps/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Sirfame Lin on 6/25/18.
+//  Copyright © 2018 Sirfame Lin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet var map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let latitude: CLLocationDegrees = 25.034
+        let longitude: CLLocationDegrees = 121.5645
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        map.setRegion(region, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
