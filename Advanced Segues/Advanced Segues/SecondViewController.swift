79f278aacc157f6906f443976b7390e39059dce3
Sirfame Lin <sirfame@hotmail.com>
2018-07-10 07:48:33 -0700

    table cell to segue, passing variables by calling prepareForSegue

--- Advanced Segues/Advanced Segues/SecondViewController.swift
+++ Advanced Segues/Advanced Segues/SecondViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    var sf = "sf"
+    var activeRow = 0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(sirfame)
+        print(sf)
+        print(activeRow)
         // Do any additional setup after loading the view.
     }
 
--- Advanced Segues/Advanced Segues/ViewController.swift
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -8,12 +8,43 @@
 
 import UIKit
 let sirfame = "Srfame"
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var activeRow = 0;
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4;
+    }
+    
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // id row that was tapped
+        activeRow = indexPath.row
+        performSegue(withIdentifier: "toSecondViewController", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondViewController" {
+            let secondViewController = segue.destination as! SecondViewController
+            
+            secondViewController.sf = "sirfame lin"
+            secondViewController.activeRow = activeRow;
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
