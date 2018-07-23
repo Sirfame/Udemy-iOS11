24330faa30a551e641cc26286b07cce61a9a9025
Sirfame Lin <sirfame@hotmail.com>
2018-07-23 08:03:26 -0700

    completed bach to bach

--- Bach to Bach/Bach to Bach/ViewController.swift
+++ Bach to Bach/Bach to Bach/ViewController.swift
@@ -11,16 +11,30 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    let audioPath = Bundle.main.path(forResource: "bach", ofType:"mp3");
     var player = AVAudioPlayer()
+    var timer = Timer()
 
     @IBAction func play(_ sender: Any) {
         player.prepareToPlay()
         
         player.play()
-        
-    }
+        timer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)
+            }
     @IBAction func pause(_ sender: Any) {
         player.pause()
+        timer.invalidate();
+    }
+    @IBAction func stop(_ sender: Any) {
+        player.pause()
+        timer.invalidate();
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            time.maximumValue = Float(player.duration)
+        } catch {
+            
+        }
+        time.value = 0;
     }
     @IBAction func volume(_ sender: Any) {
         player.volume = volumeValue.value;
@@ -29,7 +43,7 @@ class ViewController: UIViewController {
     @IBAction func changeTime(_ sender: Any) {
         player.currentTime = TimeInterval(time.value)
     }
-    func updateTime(_ timer: Timer) {
+    @objc func updateTime(_ timer: Timer) {
         time.value = Float(player.currentTime)
     }
     @IBOutlet var time: UISlider!
@@ -37,13 +51,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let audioPath = Bundle.main.path(forResource: "bach", ofType:"mp3");
         
         do {
             try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
             time.maximumValue = Float(player.duration)
-            time.value = 0.0
-            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.changeTime), userInfo: nil, repeats: true)
         } catch {
             
         }
