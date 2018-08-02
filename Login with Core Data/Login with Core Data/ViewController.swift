02e02c5696584494997cad92bfabf7ca846da979
Sirfame Lin <sirfame@hotmail.com>
2018-08-02 07:58:11 -0700

    updating username and logout functionality in login demo

--- Login with Core Data/Login with Core Data/ViewController.swift
+++ Login with Core Data/Login with Core Data/ViewController.swift
@@ -12,6 +12,73 @@ import CoreData
 class ViewController: UIViewController {
     
     
+    @IBOutlet var changeUsername: UIButton!
+    @IBAction func changeUsernameButton(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users") // sort of like an array
+        request.returnsObjectsAsFaults = false;
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if var username = result.value(forKey:"username") as? String {
+                        result.setValue(usernameField.text, forKey: "username")
+                        username = result.value(forKey:"username") as! String
+                        do {
+                            try context.save();
+                            print("username updated successfully")
+                            
+                            displayUsername.text = "Hello \(username)"
+                        } catch {
+                            print("update failed")
+                        }
+                        
+                    }
+                }
+            } else {
+                displayUsername.text = "Please login"
+            }
+        } catch {
+            print("There was an error.")
+        }
+    }
+    @IBOutlet var logout: UIButton!
+    @IBAction func logoutButton(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users") // sort of like an array
+        request.returnsObjectsAsFaults = false;
+        
+        
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let username = result.value(forKey:"username") as? String {
+                        context.delete(result)
+                        do {
+                            try context.save();
+                            print("logged out (deleted) successfully")
+                            login.alpha = 1;
+                            logout.alpha = 0;
+                            changeUsername.alpha = 0;
+                            displayUsername.text = "Please login"
+                        } catch {
+                            print("logout (delete) failed")
+                        }
+                        
+                    }
+                }
+            } else {
+                displayUsername.text = "Please login"
+            }
+        } catch {
+            print("There was an error.")
+        }
+        
+        
+    }
     @IBOutlet var login: UIButton!
     @IBOutlet var displayUsername: UILabel!
     @IBOutlet var usernameField: UITextField!
@@ -24,8 +91,8 @@ class ViewController: UIViewController {
             try context.save()
             print("saved")
             login.alpha = 0;
-            usernameField.alpha = 0;
-            
+            logout.alpha = 1;
+            changeUsername.alpha = 1;
             displayUsername.text = "Hello " + usernameField.text!
         } catch {
             print("There was an error.")
@@ -56,8 +123,8 @@ class ViewController: UIViewController {
                 for result in results as! [NSManagedObject] {
                     if let username = result.value(forKey:"username") as? String {
                         login.alpha = 0;
-                        usernameField.alpha = 0;
-                        
+                        logout.alpha = 1;
+                        changeUsername.alpha = 1;
                         displayUsername.text = "Hello \(username)"
                     }
                 }
